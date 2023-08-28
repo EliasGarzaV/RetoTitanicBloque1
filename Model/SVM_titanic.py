61a96aef84db6304bb506eb2f7d0ad176edc993f
Diego Elián Rodríguez Cantú <75437643+DiegoElian02@users.noreply.github.com>
2023-08-28 04:26:48 -0600

    reporte

--- Model/SVM_titanic.py
+++ Model/SVM_titanic.py
@@ -1,33 +1,31 @@
 import pandas as pd
 import os
-path = os.getcwd()
-import numpy as np
+
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+path = os.getcwd()
 
-train = pd.read_csv(path + '\RetoTitanicBloque1\Data\\train_clean.csv')
-train = train.drop(columns = [train.columns[0]])
+def titanic_svm(kernelf : str, degreef: int):
+    train = pd.read_csv('..\Data\\train_clean.csv')
+    train = train.drop(columns = [train.columns[0]])
 
-test = pd.read_csv(path + '\RetoTitanicBloque1\Data\\test.csv')
-idxtest = test['PassengerId']
+    X_train, X_test, y_train, y_test = train_test_split(train.drop(columns='Survived'), train['Survived'], test_size=0.3, random_state=1)
+
+    classifier = SVC(kernel=kernelf, degree = degreef)
+    classifier.fit(X_train, y_train)
+    prediction = classifier.predict(X_test)
+    
+    acc = accuracy_score(y_test, prediction)
+    
+    # Create a Confusion Matrix
+    matrix = pd.DataFrame(
+            confusion_matrix(y_test, prediction),
+            columns=['Predicted 0', 'Predicted 1'],
+            index=['Actual 0', 'Actual 1'])
+    return acc, matrix
 
 
-X_train, X_test, y_train, y_test = train_test_split(train.drop(columns='Survived'), train['Survived'], test_size=0.2, random_state=1)
-
-classifier = SVC()
-classifier.fit(X_train, y_train)
-prediction = classifier.predict(X_test)
-
-acc = accuracy_score(y_test, prediction)
-matrix = pd.DataFrame(
-        confusion_matrix(y_test, prediction),
-        columns=['Predicted 0', 'Predicted 1'],
-        index=['Actual 0', 'Actual 1'])
-
-print(matrix)
-
-print(classification_report(y_test,prediction))
-
+#train = pd.read_csv(path + '\RetoTitanicBloque1\Data\\train_clean.csv')
+#print(train)
 #pd.DataFrame({'PassengerId' : idxtest, 'Survived' : prediction}).to_csv(path + '\RetoTitanicBloque1\Results\SVM_gender_submission.csv', index = False)
